91ea42e81b16918d8f367e441709b746b7f8297f
Chaitanya Vuppuluri <vuppuluri@gmail.com>
2025-08-31 11:39:55 +0530

    Standardized middleware

---
+++ cmd/test-service/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	handlers "chaits.org/go-microservices-repo/internal/handlers/test-service"
+	appserver "chaits.org/go-microservices-repo/internal/server"
+	"chaits.org/go-microservices-repo/pkg/general/logger"
+	"chaits.org/go-microservices-repo/pkg/general/tracing"
+	"chaits.org/go-microservices-repo/pkg/network/middleware"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var serviceName = "test-service"
+
+func main() {
+
+	logger.Init(serviceName)
+
+	shutdownTracer := tracing.InitTracer(context.Background(), serviceName)
+	defer shutdownTracer()
+
+	middlewares := middleware.NewManager(middleware.WithLogging, middleware.WithPrometheusMetrics(serviceName), middleware.WithCORS)
+
+	http.Handle("/hello", middlewares.Then(handlers.HelloHandler, "hello-handler"))
+	http.Handle("/chain", middlewares.Then(handlers.ChainHandler, "chain-handler"))
+
+	// http.Handle("/hello", otelhttp.NewHandler(middlewares.Then1(handlers.HelloHandler), "hello-handler"))
+	// http.Handle("/chain", otelhttp.NewHandler(middlewares.Then1(handlers.ChainHandler), "chain-handler"))
+
+	// http.Handle("/hello", otelhttp.NewHandler(middleware.ChainAllHandlers(handlers.HelloHandler, serviceName), "hello-handler"))
+	// http.Handle("/chain", otelhttp.NewHandler(middleware.ChainAllHandlers(handlers.ChainHandler, serviceName), "chain-handler"))
+
+	server := &http.Server{Addr: ":8080"}
+	http.Handle("/metrics", promhttp.Handler())
+	appserver.StartServer(serviceName, server)
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func chiRouter() {
+	// r := chi.NewRouter()
+	// r.Use(middleware.WithPrometheusMetrics, middleware.WithLogging)
+	// r.Method(http.MethodGet, "/hello", otelhttp.NewHandler(middleware.ChainAllHandlers(helloHandler), "hello-handler"))
+	// r.Method(http.MethodGet, "/chain", otelhttp.NewHandler(middleware.ChainAllHandlers(chainHandler), "chain-handler"))
+	// http.Handle("/", r)
+	// http.Handle("/metrics", promhttp.Handler())
+	// 	fmt.Println("Server starting on port 8080 with OpenTelemetry tracing...")
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+}
